95bbdf7e9735e424c5ada246fb9900cfd9e59709
Syahrul <syahrul4w@gmail.com>
2023-11-13 13:18:16 +0700

    setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'laptop3': {
+        'min': '1280px',
+        'max': '1439px'
+      },
+
+      'laptop1': {
+        'min': '1440px',
+        'max': '1500px'
+      },
+
+      'laptop2': {
+        'raw': '(width: 1280px) and (height: 800px)'
+      },
+    },
+    plugins: [],
+  }
+}
