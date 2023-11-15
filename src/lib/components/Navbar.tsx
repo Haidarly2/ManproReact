0d577cc8c5d1ab2fd2f15a7c66e914fd5304b9ae
Syahrul <syahrul4w@gmail.com>
2023-11-15 07:54:01 +0700

    Update navbar

--- src/lib/components/Navbar.tsx
+++ src/lib/components/Navbar.tsx
@@ -7,20 +7,16 @@ import list from "../assets/List.svg";
 export const Navbar: React.FC<navbarInterface> = ({ section }) => {
   return (
     <nav className="fixed w-full bottom-0 bg-[#618264] rounded-t-[2rem] border border-black flex justify-center p-4 pb-0">
-      <ul className="flex gap-[10rem] justify-center relative">
-        <li className="cursor-pointer" onClick={() => section("home")}>
+      <ul className="flex gap-[14rem] justify-center relative mb-4">
+        <li className="navbarLi" onClick={() => section("home")}>
           <img src={home} alt="home" />
         </li>
         <li
-          className="absolute bottom-1/4 cursor-pointer"
+          className="absolute bottom-1/4 navbarLi bg-[#B0D9B1] rounded-full p-5 border border-black"
           onClick={() => section("add")}>
-          <img
-            src={add}
-            alt=""
-            className="bg-[#B0D9B1] rounded-full p-4 border border-black"
-          />
+          <img src={add} alt="add" />
         </li>
-        <li className="cursor-pointer" onClick={() => section("list")}>
+        <li className="navbarLi" onClick={() => section("list")}>
           <img src={list} alt="list" />
         </li>
       </ul>
